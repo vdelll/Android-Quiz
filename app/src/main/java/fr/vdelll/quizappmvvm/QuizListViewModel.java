5d7af66cbc440821bb9d8e33d5d1b85bff5f2fc7
Vdelll <contact@vdelll.fr>
2020-07-18 16:08:15 +0200

    Creating Firebase Repository

---
+++ app/src/main/java/fr/vdelll/quizappmvvm/QuizListViewModel.java
@@ -0,0 +1,9 @@
+package fr.vdelll.quizappmvvm;
+
+import androidx.lifecycle.ViewModel;
+
+public class QuizListViewModel extends ViewModel {
+
+    
+
+}
--- app/src/main/java/fr/vdelll/quizappmvvm/StartFragment.java
+++ app/src/main/java/fr/vdelll/quizappmvvm/StartFragment.java
@@ -2,57 +2,39 @@ package fr.vdelll.quizappmvvm;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 public class StartFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ProgressBar startProgress;
+    private TextView startFeedBackText;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FirebaseAuth firebaseAuth;
+    private static final String START_TAG = "START_LOG";
+
+    private NavController navController;
 
     public StartFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StartFragment newInstance(String param1, String param2) {
-        StartFragment fragment = new StartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -60,5 +42,48 @@ public class StartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        navController = Navigation.findNavController(view);
+
+        startProgress = view.findViewById(R.id.start_progress);
+        startFeedBackText = view.findViewById(R.id.start_feedback);
+
+        startFeedBackText.setText("Checking user account...");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser == null){
+
+            startFeedBackText.setText("Creating account...");
+
+            // Create new account
+            firebaseAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        startFeedBackText.setText("Account Created...");
+                        navController.navigate(R.id.action_startFragment_to_listFragment);
+                    } else {
+                        Log.d(START_TAG, "Start log : " + task.getException());
+                    }
+                }
+            });
+        } else {
+            // Navigate to homepage
+            startFeedBackText.setText("Logged In...");
+            navController.navigate(R.id.action_startFragment_to_listFragment);
+        }
+
     }
 }
