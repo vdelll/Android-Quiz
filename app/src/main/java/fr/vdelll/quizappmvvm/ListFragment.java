c84758cbabcc45e32c0a2a0b880dd94a41dda029
Vdelll <contact@vdelll.fr>
2020-07-18 16:37:24 +0200

    Creating adapter

--- app/src/main/java/fr/vdelll/quizappmvvm/ListFragment.java
+++ app/src/main/java/fr/vdelll/quizappmvvm/ListFragment.java
@@ -5,15 +5,23 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class ListFragment extends Fragment {
 
     private RecyclerView listView;
+    private QuizListViewModel quizListViewModel;
+
+    private QuizListAdapter adapter;
 
     public ListFragment() {
         // Required empty public constructor
@@ -30,5 +38,23 @@ public class ListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         listView = view.findViewById(R.id.list_view);
+        adapter = new QuizListAdapter();
+
+        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+        listView.setHasFixedSize(true);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        quizListViewModel = new ViewModelProvider(getActivity()).get(QuizListViewModel.class);
+        quizListViewModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModelList) {
+                adapter.setQuizListModels(quizListModelList);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/fr/vdelll/quizappmvvm/QuizListViewModel.java
+++ app/src/main/java/fr/vdelll/quizappmvvm/QuizListViewModel.java
@@ -1,9 +1,32 @@
 package fr.vdelll.quizappmvvm;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class QuizListViewModel extends ViewModel {
+import java.util.List;
 
-    
+public class QuizListViewModel extends ViewModel implements FirebaseRepository.OnFirestoreTaskComplete {
 
+    private MutableLiveData<List<QuizListModel>> quizListModelData = new MutableLiveData<>();
+
+    private FirebaseRepository firebaseRepository = new FirebaseRepository(this);
+
+    public QuizListViewModel() {
+        firebaseRepository.getQuizData();
+    }
+
+    @Override
+    public void quizListDataAdded(List<QuizListModel> quizListModelList) {
+        quizListModelData.setValue(quizListModelList);
+    }
+
+    @Override
+    public void onError(Exception e) {
+
+    }
+
+    public LiveData<List<QuizListModel>> getQuizListModelData() {
+        return quizListModelData;
+    }
 }
